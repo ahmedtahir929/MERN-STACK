72f994d05546bd979e4e7fc252367181c0ce58b3
ahmedtahir929 <ahmedtahir929pk@gmail.com>
2026-06-05 09:54:00 +0500

    Feat: Integrated Vercel Analytics tracking script to frontend layout root

--- SSPMC - Smart Services Portal for Multan Citizens/frontend/src/main.jsx
+++ SSPMC - Smart Services Portal for Multan Citizens/frontend/src/main.jsx
@@ -1,35 +1,20 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import { ThemeProvider } from './ContextAPIs/ThemeContext.jsx';
-
+import { Analytics } from '@vercel/analytics/react';
 import App from './App.jsx';
-import api from './utils/apiConfig';
-
 import 'react-toastify/dist/ReactToastify.css';
 import './index.css';
-
-const token = localStorage.getItem('token');
-if (token) {
-  api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <ThemeProvider>
         <App />
-        <ToastContainer 
-          position="top-right"
-          autoClose={3000}
-          hideProgressBar={false}
-          newestOnTop={true}
-          closeOnClick
-          pauseOnHover
-          draggable
-          theme="colored" 
-        />
+        <Analytics />
+        <ToastContainer theme="colored" />
       </ThemeProvider>
     </BrowserRouter>
   </StrictMode>,
