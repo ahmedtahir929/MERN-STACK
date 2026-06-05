47649ef3814ac9e8033ff36993a0bdefb86aa1e3
ahmedtahir929 <ahmedtahir929pk@gmail.com>
2026-06-05 09:57:24 +0500

    fixed main.jsx token bugs fixed due to uncessary update to main

--- SSPMC - Smart Services Portal for Multan Citizens/frontend/src/main.jsx
+++ SSPMC - Smart Services Portal for Multan Citizens/frontend/src/main.jsx
@@ -1,12 +1,20 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 import { ThemeProvider } from './ContextAPIs/ThemeContext.jsx';
 import { Analytics } from '@vercel/analytics/react';
+
 import App from './App.jsx';
+import api from './utils/apiConfig';
+
 import 'react-toastify/dist/ReactToastify.css';
 import './index.css';
+
+const token = localStorage.getItem('token');
+if (token) {
+  api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -14,7 +22,16 @@ createRoot(document.getElementById('root')).render(
       <ThemeProvider>
         <App />
         <Analytics />
-        <ToastContainer theme="colored" />
+        <ToastContainer 
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={true}
+          closeOnClick
+          pauseOnHover
+          draggable
+          theme="colored" 
+        />
       </ThemeProvider>
     </BrowserRouter>
   </StrictMode>,
