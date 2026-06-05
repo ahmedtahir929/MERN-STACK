7e792ec5c36c4bc6556edca95daec615ed1d712a
ahmedtahir929 <ahmedtahir929pk@gmail.com>
2026-06-05 22:24:19 +0500

    Enabled performance insights - vercel

--- SSPMC - Smart Services Portal for Multan Citizens/frontend/src/main.jsx
+++ SSPMC - Smart Services Portal for Multan Citizens/frontend/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import { ThemeProvider } from './ContextAPIs/ThemeContext.jsx';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 import App from './App.jsx';
 import api from './utils/apiConfig';
@@ -22,6 +23,7 @@ createRoot(document.getElementById('root')).render(
       <ThemeProvider>
         <App />
         <Analytics />
+        <SpeedInsights />
         <ToastContainer 
           position="top-right"
           autoClose={3000}
